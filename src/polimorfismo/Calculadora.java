7c914ac592e13a82a7e78f4ef67ec74ec4f7d19a
Anderson Mazzotti <anderson.mazzotti@alunos.sc.senac.br>
2022-09-19 20:21:49 -0300

    Polimorfismo

---
+++ src/polimorfismo/Calculadora.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polimorfismo;
+
+/**
+ *
+ * @author anderson.mazzotti
+ */
+public class Calculadora {
+    
+    
+    public int somar(int v1, int v2){
+     
+        int soma = v1+v2;
+        return soma;
+    }
+    
+    public int somar(int v1, int v2, int v3){
+     
+        int soma = v1+v2+v3;
+        return soma;
+    }
+    
+     public double somar(double v1, double v2, double v3){
+     
+        double soma = v1+v2;
+        return soma;
+    }
+    
+    
+    
+}
